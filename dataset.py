7556a1b7be2a8547017006344168d7c502c22a7f
Manu Harju <manu.harju@gmail.com>
2022-12-21 15:18:42 +0200

    variable length datasets, collate_fn and masking

--- dataset.py
+++ dataset.py
@@ -6,12 +6,12 @@ from typing import List, Union, Optional
 
 
 class RawAudioDataset(torch.utils.data.Dataset):
-    def __init__(self, filenames: List[Union[str, Path]], sample_rate: int = 16000, max_duration: float = 10.0):
+    def __init__(self, filenames: List[Union[str, Path]], sample_rate: int = 16000, max_duration: Optional[float] = None):
         super().__init__()
 
         self.filenames = filenames
         self.sample_rate = sample_rate
-        self.max_length = int(max_duration * sample_rate)
+        self.max_length = int(max_duration * sample_rate) if max_duration is not None else None
 
     def __len__(self):
         return len(self.filenames)
@@ -25,7 +25,7 @@ class RawAudioDataset(torch.utils.data.Dataset):
         if sr != self.sample_rate:
             y = torchaudio.functional.resample(y, orig_freq=sr, new_freq=self.sample_rate)
 
-        if len(y) > self.max_length:
+        if self.max_length is not None and len(y) > self.max_length:
             offset = torch.randint(y.size(-1) - self.max_length, size=(1,))[0]
             y = y[offset:offset + self.max_length]
 
@@ -39,7 +39,7 @@ class MelDataset(torch.utils.data.Dataset):
                  n_mels: int = 64,
                  n_fft: int = 1024,
                  hop_length: Optional[int] = None,
-                 max_length: int = 320,
+                 max_length: Optional[int] = None,
                  log_mel_scaling: float = 0.1):
 
         super().__init__()
@@ -71,11 +71,47 @@ class MelDataset(torch.utils.data.Dataset):
         mels = self.melspec(y)
         # (n_mels, t)
 
-        if mels.size(-1) > self.max_length:
+        if self.max_length is not None and mels.size(-1) > self.max_length:
             offset = torch.randint(mels.size(-1) - self.max_length, size=(1,))[0]
             mels = mels[:, offset:offset + self.max_length]
 
         log_mels = self.scaling * torch.log(mels + torch.finfo(torch.float32).eps)
 
         return log_mels
+
+
+def collate_fn(batch):
+    # assume the time dimension is the last one, and the others dims are staying constant
+    batch_size = len(batch)
+    dim = list(batch[0].shape[:-1])
+    lengths = [x.size(-1) for x in batch]
+    
+    x = torch.zeros([batch_size, max(lengths)] + dim, dtype=batch[0].dtype)
+    mask = -float('inf') * torch.ones(batch_size, max(lengths))
+
+    for k in range(batch_size):
+        x[k, :lengths[k]] = batch[k].permute(1, 0) if batch[k].dim() == 2 else batch[k]
+        mask[k, :lengths[k]] = 0
+
+    if len(dim) > 0:
+        x = x.permute(0, 2, 1)
+
+    return x, mask
+    
+
+def test_datasets():
+    fns = sorted(Path('/home/work/mnu/data/clotho/audio/validation').glob('*.wav'))
+
+    #ds = RawAudioDataset(fns)
+    ds = MelDataset(fns)
+
+    loader = torch.utils.data.DataLoader(ds, batch_size=16, collate_fn=collate_fn, shuffle=True)
+
+    x, mask = next(iter(loader))
+    print(x.shape, mask.shape)
+    
+    print(torch.sum(mask == 0) / mask.numel())
+
+if __name__ == '__main__':
+    test_datasets()
 
